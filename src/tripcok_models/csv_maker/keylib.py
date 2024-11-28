4c1a2d1e025f08169217ce400ac36a22c373dc60
lsiwh37249 <lsiwh37249@gmail.com>
2024-11-28 10:27:02 +0900

    batch 48까지

--- src/tripcok_models/csv_maker/keylib.py
+++ src/tripcok_models/csv_maker/keylib.py
@@ -1,28 +1,19 @@
-# RoundRobin 클래스 정의
-class RoundRobin:
-    def __init__(self, items):
-        self.items = items
-        self.index = -1
+count = -1
 
-    def next(self):
-        """다음 아이템 반환"""
-        if not self.items:
-            return None
-        self.index = (self.index + 1) % len(self.items)
-        return self.items[self.index]
-
-# 환경 변수에서 API 키를 가져오는 함수
 def get_keys():
+    global count
+
     import os
     API_KEY1 = os.getenv("TOURISM_API1")
     API_KEY2 = os.getenv("TOURISM_API2")
     API_KEY3 = os.getenv("TOURISM_API3")
-    API_KEY4 = os.getenv("TOURISM_API4")
-    return [key for key in [API_KEY1, API_KEY2, API_KEY3, API_KEY4] if key]  # None 필터링
+    #API_KEY4 = os.getenv("TOURISM_API4")
+    key = [API_KEY1, API_KEY2, API_KEY3]
+   
+    key = [k for k in key if k]
 
-# get_keys() 함수 호출로 반환 값을 전달
-round_robin = RoundRobin(get_keys())
+    if not key:  # 키가 없으면 예외 발생
+        raise ValueError("환경 변수에서 유효한 API 키를 찾을 수 없습니다!")
 
-# API 키 순환 출력 테스트
-for _ in range(10):
-    print(round_robin.next())
+    count = (count + 1) % len(key)  # 라운드 로빈 인덱스 업데이트
+    return key[count]
---
+++ src/tripcok_models/csv_maker/versions_past/keylib.py
@@ -0,0 +1,19 @@
+count = -1
+
+def get_keys():
+    global count
+
+    import os
+    API_KEY1 = os.getenv("TOURISM_API1")
+    API_KEY2 = os.getenv("TOURISM_API2")
+    API_KEY3 = os.getenv("TOURISM_API3")
+    #API_KEY4 = os.getenv("TOURISM_API4")
+    key = [API_KEY1, API_KEY2, API_KEY3]
+   
+    key = [k for k in key if k]
+
+    if not key:  # 키가 없으면 예외 발생
+        raise ValueError("환경 변수에서 유효한 API 키를 찾을 수 없습니다!")
+
+    count = (count + 1) % len(key)  # 라운드 로빈 인덱스 업데이트
+    return key[count]
