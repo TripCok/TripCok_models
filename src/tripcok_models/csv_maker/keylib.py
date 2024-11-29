da581adad759b318a1bcf74df77a3cad62bdfe17
lsiwh37249 <lsiwh37249@gmail.com>
2024-11-29 20:39:33 +0900

    batch 142

--- src/tripcok_models/csv_maker/keylib.py
+++ src/tripcok_models/csv_maker/keylib.py
@@ -11,8 +11,8 @@ def get_keys():
     API_KEY5 = os.getenv("TOURISM_API5")
     API_KEY6 = os.getenv("TOURISM_API6")
     
-    #key = [API_KEY1, API_KEY2, API_KEY3]
-    key = [API_KEY4, API_KEY5, API_KEY6]
+    key = [API_KEY1, API_KEY2, API_KEY3]
+    #key = [API_KEY4, API_KEY5, API_KEY6]
     #key = [API_KEY1, API_KEY2, API_KEY3]
     
     key = [k for k in key if k]
