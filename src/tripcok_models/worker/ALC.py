f29517b88fee3dd1e386491d9e72b6ece67dbc56
Jeonghoon2 <ukidd12@naver.com>
2024-11-30 01:50:50 +0900

    [FEAT] 카테고리 자동 생성 기능

---
+++ src/tripcok_models/worker/ALC.py
@@ -0,0 +1,60 @@
+import csv
+from request_utils import *
+
+CSV_FILE = "./category.csv"
+ADMIN_ID = 1
+API_URL = "http://localhost:8080/api/v1/place/category"
+
+with open(CSV_FILE, "r") as f:
+    reader = csv.reader(f)
+    csv_list = list(reader)
+
+for row in csv_list:
+    # 첫번째 카테고리 검사
+    first_category_id = None
+
+    # 첫번째 카테고리에 같은 이름이 있는지 없는지 검사
+    first_search_data = {"depth": 0, "categoryName": row[0], }
+    first_request = get_api_request(API_URL, "param", first_search_data)
+
+    if len(first_request) == 0:
+        # 첫번째 카테고리 생성
+        api_request = post_api_request(API_URL, {"placeName": str(row[0]), "memberId": ADMIN_ID})
+        first_category_id = api_request["id"]
+        print(f"CREATE : category = {api_request['name']}")
+    else:
+        first_category_id = first_request[0]["id"]
+
+    # 두번째 카테고리 검사
+    second_category_id = None
+
+    # 부모 카테고리 자식에 같은 이름이 있는지 없는지 검사
+    second_search_data = {"depth": 1, "categoryName": row[1], "parentId": first_category_id}
+    second_request = get_api_request(API_URL, "param", second_search_data)
+
+    if len(second_request) == 0:
+        # 두번째 카테고리 생성
+        api_request = post_api_request(API_URL, {"parentId": first_category_id, "placeName": str(row[1]),
+                                                 "memberId": ADMIN_ID})
+        second_category_id = api_request["id"]
+        print(f"CREATE : category = {api_request['name']}")
+    else:
+        second_category_id = second_request[0]["id"]
+
+
+
+    # 세번째 카테고리 검사
+    third_category_id = None
+
+    # 부모 카테고리 자식에 같은 이름이 있는지 없는지 검사
+    third_category_data = {"depth": 2, "categoryName": row[2], "parentId": second_category_id}
+    third_request = get_api_request(API_URL, "param", third_category_data)
+
+    if len(third_request) == 0:
+        # 두번째 카테고리 생성
+        api_request = post_api_request(API_URL, {"parentId": second_category_id, "placeName": str(row[2]),
+                                                 "memberId": ADMIN_ID})
+        second_category_id = api_request["id"]
+        print(f"CREATE : category = {api_request['name']}")
+    else:
+        second_category_id = third_request[0]["id"]
